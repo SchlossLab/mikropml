16a4c354b7edf310f498cef9c6de5a64ba3083aa
William L. Close <close.will@gmail.com>
2020-01-08 10:36:33 -0500

    making L1 linear slurm script

--- code/slurm/L1_Linear_SVM.sh
+++ code/slurm/L1_Linear_SVM.sh
@@ -1,35 +1,67 @@
+#!/bin/bash
 
-#!/bin/sh
-#PBS -l procs=1,pmem=4gb
-#PBS -l walltime=20:00:00
-#PBS -j oe
-#PBS -m a
-#PBS -V
-#PBS -M begumtop@umich.edu
-#PBS -A pschloss_fluxod
-#PBS -q fluxod
-#PBS -l qos=flux
-#PBS -t 1-100
+###############################
+#                             #
+#  1) Job Submission Options  #
+#                             #
+###############################
+
+# Name
+#SBATCH --job-name=decisionTree
+
+# Resources
+# For MPI, increase ntasks-per-node
+# For multithreading, increase cpus-per-task
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=4GB
+#SBATCH --time=20:00:00
+
+# Account
+#SBATCH --account=pschloss1
+#SBATCH --partition=standard
+
+# Logs
+#SBATCH --mail-user=begumtop@umich.edu
+#SBATCH --mail-type=FAIL
+
+# Environment
+##SBATCH --array=1-100
+##SBATCH --export=ALL
+
+# --array is array parameter, the same job will be submitted the length of the input,
+# each with its own unique array id ($SLURM_ARRAY_TASK_ID)
+
+# Connecting node to internet
+source /etc/profile.d/http_proxy.sh 
+
+# List compute nodes allocated to the job
+if [[ $SLURM_JOB_NODELIST ]] ; then
+	echo "Running on"
+	scontrol show hostnames $SLURM_JOB_NODELIST
+	echo -e "\n"
+fi
+
 
 # Load Modules:
 #  1) R/3.5.0
 #  2)  r-biomed-libs
 #  3) run Rscript code/learning/load_caret_models_flux.R
 
-# -t is array parameter, the same job will be submitted the length of the input,
-# each with its own unique array id ($PBS_ARRAYID)
 
 
-cat $PBS_NODEFILE
-qstat -f $PBS_JOBID
 
-cd $PBS_O_WORKDIR
+#####################
+#                   #
+#  2) Job Commands  #
+#                   #
+#####################
+
 # vector index starts at 0 so shift array by one
-cd $PBS_O_WORKDIR
-seed=$(($PBS_ARRAYID - 1))
+seed=$(($SLURM_ARRAY_TASK_ID - 1))
 
 # print out which model is being run in each job
-
 echo Using "L1 Linear SVM"
 
 # Using $PBS_ARRAYID to select parameter set
