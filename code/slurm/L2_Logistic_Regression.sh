3d863b537fb4fd03b568499076d55fdb2a3f5fc9
William L. Close <close.will@gmail.com>
2020-01-08 10:41:20 -0500

    making L2 logistic slurm script

--- code/slurm/L2_Logistic_Regression.sh
+++ code/slurm/L2_Logistic_Regression.sh
@@ -1,41 +1,72 @@
-#!/bin/sh
-#PBS -l procs=1,pmem=4gb
-#PBS -l walltime=20:00:00
-#PBS -j oe
-#PBS -m a
-#PBS -V
-#PBS -M begumtop@umich.edu
-#PBS -A pschloss_fluxod
-#PBS -q fluxod
-#PBS -l qos=flux
-#PBS -t 1-100
+#!/bin/bash
+
+###############################
+#                             #
+#  1) Job Submission Options  #
+#                             #
+###############################
+
+# Name
+#SBATCH --job-name=L2Logistic
+
+# Resources
+# For MPI, increase ntasks-per-node
+# For multithreading, increase cpus-per-task
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=4GB
+#SBATCH --time=20:00:00
+
+# Account
+#SBATCH --account=pschloss1
+#SBATCH --partition=standard
+
+# Logs
+#SBATCH --mail-user=begumtop@umich.edu
+#SBATCH --mail-type=FAIL
+
+# Environment
+##SBATCH --array=1-100
+##SBATCH --export=ALL
+
+# --array is array parameter, the same job will be submitted the length of the input,
+# each with its own unique array id ($SLURM_ARRAY_TASK_ID)
+
+# Connecting node to internet
+source /etc/profile.d/http_proxy.sh 
+
+# List compute nodes allocated to the job
+if [[ $SLURM_JOB_NODELIST ]] ; then
+	echo "Running on"
+	scontrol show hostnames $SLURM_JOB_NODELIST
+	echo -e "\n"
+fi
+
 
 # Load Modules:
 #  1) R/3.5.0   2) r-biomed-libs/3.5.0
 
-# -t is array parameter, the same job will be submitted the length of the input,
-# each with its own unique array id ($PBS_ARRAYID)
 
 
-cat $PBS_NODEFILE
-qstat -f $PBS_JOBID
 
-cd $PBS_O_WORKDIR
+#####################
+#                   #
+#  2) Job Commands  #
+#                   #
+#####################
 
 # vector index starts at 0 so shift array by one
-
-seed=$(($PBS_ARRAYID - 1))
+seed=$(($SLURM_ARRAY_TASK_ID - 1))
 
 # print out which model is being run in each job
-
 echo Using "L2 Logistic Regression"
 
 # Using $PBS_ARRAYID to select parameter set
-
 make data/temp/best_hp_results_L2_Logistic_Regression_$seed.csv
 
 echo "Script complete"
-echo "qsub working directory absolute is"
-echo $PBS_O_WORKDIR
-qstat -f $PBS_JOBID
+echo "sbatch working directory is"
+echo $SLURM_SUBMIT_DIR
+scontrol show job $SLURM_ARRAY_JOB_ID
 exit
