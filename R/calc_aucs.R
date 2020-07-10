0b267fb4e3158925044b37dc4a58371438cb27f2
Kelly Sovacool <kellysovacool@gmail.com>
2020-07-10 16:38:04 -0400

    Prepare medium & large-sized OTU datasets

--- R/calc_aucs.R
+++ R/calc_aucs.R
@@ -1,37 +1,36 @@
 # Code written by Zena Lapp
 # Calculate auroc and auprc
 
-#' Title
+#' Get predictions from trained model and test data
 #'
 #' @param trained_model TODO
 #' @param test_data TODO
-#' @param fewer_samples TODO
+#' @param outcome_samples TODO
 #'
 #' @return
 #' @export
 #' @author Zena Lapp, \email{zenalapp@@umich.edu}
 #'
-get_pred <- function(trained_model, test_data, fewer_samples) {
-  # get predictions
-  pred <- stats::predict(trained_model, test_data, type = "prob")[[fewer_samples]]
+get_pred <- function(trained_model, test_data, outcome_samples) {
+  pred <- stats::predict(trained_model, test_data, type = "prob")[[outcome_samples]]
 }
 
-#' Title
+#' Get binary outcome vector for calculating AUCs
 #'
 #' @param outcome_vec TODO
-#' @param fewer_samples TODO
+#' @param outcome_samples TODO
 #'
 #' @return
 #' @export
 #' @author Zena Lapp, \email{zenalapp@@umich.edu}
 #'
 #'
-get_bin_outcome <- function(outcome_vec, fewer_samples) {
-  # get binary outcome to calculate aucs (1 is outcome with fewer samples, 0 is outcome with more samples)
-  bin_outcome <- ifelse(outcome_vec == fewer_samples, 1, 0)
+get_bin_outcome <- function(outcome_vec, outcome_samples) {
+  # 1 is outcome with fewer samples, 0 is outcome with more samples
+  bin_outcome <- ifelse(outcome_vec == outcome_samples, 1, 0)
 }
 
-#' Title
+#' Calculate AUROC
 #'
 #' @param pred TODO
 #' @param bin_outcome TODO
@@ -42,11 +41,10 @@ get_bin_outcome <- function(outcome_vec, fewer_samples) {
 #'
 #'
 calc_auroc <- function(pred, bin_outcome) {
-  # calculate auroc
   auroc <- PRROC::roc.curve(pred, weights.class0 = bin_outcome)$auc
 }
 
-#' Title
+#' Calculate AUPRC
 #'
 #' @param pred TODO
 #' @param bin_outcome TODO
@@ -57,27 +55,25 @@ calc_auroc <- function(pred, bin_outcome) {
 #'
 #'
 calc_auprc <- function(pred, bin_outcome) {
-  # calculate auprc
   auprc <- PRROC::pr.curve(pred, weights.class0 = bin_outcome)$auc.integral
 }
 
-# wrapper function to calculate aucs
-#' Title
+#' Calculate AUROC and AUPRC
 #'
 #' @param trained_model TODO
 #' @param test_data TODO
 #' @param outcome TODO
-#' @param fewer_samples TODO
+#' @param outcome_samples TODO
 #'
 #' @return
 #' @export
 #' @author Zena Lapp, \email{zenalapp@@umich.edu}
 #'
 #'
-calc_aucs <- function(trained_model, test_data, outcome, fewer_samples) {
-  pred <- get_pred(trained_model, test_data, fewer_samples)
+calc_aucs <- function(trained_model, test_data, outcome, outcome_samples) {
+  pred <- get_pred(trained_model, test_data, outcome_samples)
   outcome_vec <- test_data[, outcome]
-  bin_outcome <- get_bin_outcome(outcome_vec, fewer_samples)
+  bin_outcome <- get_bin_outcome(outcome_vec, outcome_samples)
   auroc <- calc_auroc(pred, bin_outcome)
   auprc <- calc_auprc(pred, bin_outcome)
   return(list(auroc = auroc, auprc = auprc))
---
+++ R/data.R
@@ -0,0 +1,17 @@
+#' Medium-sized OTU abundance dataset
+#'
+#' A dataset containing relatives abundances of 6,920 OTUs for 60 human stool samples
+#'
+#' @format A data frame with 60 rows and 6921 variables.
+#' The `dx` column is the diagnosis: healthy or cancerous (colorectal).
+#' All other columns are OTU relative abundances.
+"otu_medium"
+
+#' Large OTU abundance dataset
+#'
+#' A dataset containing relatives abundances of 6,920 OTUs for 490 human stool samples
+#'
+#' @format A data frame with 490 rows and 6,921 variables.
+#' The `dx` column is the diagnosis: healthy or cancerous (colorectal).
+#' All other columns are OTU relative abundances.
+"otu_large"
