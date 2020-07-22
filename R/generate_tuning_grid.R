53703b66ed18108059ba01eb815d9ecc48f19495
Begum Topcuoglu <begum.topcuoglu@merck.com>
2020-07-22 11:15:56 -0400

    split 1 to 2 functions

--- R/generate_tuning_grid.R
+++ R/generate_tuning_grid.R
@@ -1,56 +1,24 @@
-#' Title
+#' Generate the Tuning Grid for Tuning Hyperparameters
 #'
-#' @param train_data TODO
-#' @param model TODO
-#' @param outcome TODO
-#' @param hyperparameters TODO
+#' @param model Model name
+#' @param hyperparameters Dataframe with hyperparameters. This dataframe should have the first column named as "param" which is the hyperparameter name and the second column "val" which are the values to be tested and third column "model" which is the model being used.
 #'
-#' @return
+#' @return List of the grid and the training package being used.
 #' @export
 #' @author Begüm Topçuoğlu, \email{topcuoglu.begum@@gmail.com}
 #'
 #'
-tuning_grid <- function(train_data, model, outcome, hyperparameters) {
+#' @examples
+#' generate_tuning_grid("L2_Logistic_Regression", hp)
+generate_tuning_grid <- function(model, hyperparameters) {
 
-  # NOTE: Hyperparameters should be a csv containing a dataframe
-  # where the first column "param" is the hyperparameter name
-  # and the second column "val" are the values to be tested
-  # and third column "model" is the model being used
   hyperparameters <- hyperparameters[hyperparameters$model == model, ]
   hyperparameters <- split(hyperparameters$val, hyperparameters$param)
-
-  # set outcome as first column if null
-  # if(is.null(outcome)){
-  # outcome <- colnames(train_data)[1]
-  # }
-
-  # -------------------------CV method definition--------------------------------------->
-  # ADDED cv index to make sure
-  #     1. the internal 5-folds are stratified for diagnosis classes
-  #     2. Resample the dataset 100 times for 5-fold cv to get robust hp.
-  # IN trainControl function:
-  #     1. Train the model with final hp decision to use model to predict
-  #     2. Return 2class summary and save predictions to calculate cvROC
-  #     3. Save the predictions and class probabilities/decision values.
-  folds <- 5
-  cvIndex <- caret::createMultiFolds(factor(train_data[, outcome]), folds, times = 100)
-  cv <- caret::trainControl(
-    method = "repeatedcv",
-    number = folds,
-    index = cvIndex,
-    returnResamp = "final",
-    classProbs = TRUE,
-    summaryFunction = caret::twoClassSummary,
-    indexFinal = NULL,
-    savePredictions = TRUE
-  )
-  # # ----------------------------------------------------------------------->
-
 
 
   # -------------------Classification Method Definition---------------------->
 
-  # ---------------------------------1--------------------------------------->
+  # ------------------------------------------------------------------------->
   # For linear models we are using LiblineaR package
   #
   # LiblineaR can produce 10 types of (generalized) linear models:
@@ -80,38 +48,6 @@ tuning_grid <- function(train_data, model, outcome, hyperparameters) {
   #  • 11 – L2-regularized L2-loss support vector regression (primal)
   #  • 12 – L2-regularized L2-loss support vector regression (dual)
   #  • 13 – L2-regularized L1-loss support vector regression (dual)
-  # ------------------------------------------------------------------------>
-
-
-  # ---------------------------------2--------------------------------------->
-  # We use ROC metric for all the models
-  # To do that I had to make changes to the caret package functions.
-  # The files 'data/caret_models/svmLinear3.R and svmLinear5.R are my functions.
-  # I added 1 line to get Decision Values for linear SVMs:
-  #
-  #           prob = function(modelFit, newdata, submodels = NULL){
-  #             predict(modelFit, newdata, decisionValues = TRUE)$decisionValues
-  #           },
-  #
-  # This line gives decision values instead of probabilities and computes ROC in:
-  #   1. train function with the cross-validataion
-  #   2. final trained model
-  # using decision values and saves them in the variable "prob"
-  # This allows us to pass the cv function for all models:
-  # cv <- trainControl(method="repeatedcv",
-  #                   repeats = 100,
-  #                   number=folds,
-  #                   index = cvIndex,
-  #                   returnResamp="final",
-  #                   classProbs=TRUE,
-  #                   summaryFunction=twoClassSummary,
-  #                   indexFinal=NULL,
-  #                   savePredictions = TRUE)
-  #
-  # There parameters we pass for L1 and L2 SVM:
-  #                   classProbs=TRUE,
-  #                   summaryFunction=twoClassSummary,
-  # are actually computing ROC from decision values not probabilities
   # ------------------------------------------------------------------------>
 
   # Grid and caret method defined for each classification models
@@ -173,7 +109,6 @@ tuning_grid <- function(train_data, model, outcome, hyperparameters) {
   # Return:
   #     1. the hyper-parameter grid to tune
   #     2. the caret function to train with
-  #     3, cv method
-  params <- list(grid = grid, method = method, cv = cv)
+  params <- list(grid = grid, method = method)
   return(params)
 }
