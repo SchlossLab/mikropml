c25c4207ebd25289ecad66b3c519f44f547d0d5f
Kelly Sovacool <kellysovacool@gmail.com>
2020-07-22 14:53:50 -0400

    Fix check note with .data pronoun import

--- R/get_corr_feats.R
+++ R/get_corr_feats.R
@@ -10,6 +10,7 @@
 #'
 #' @examples
 #' flatten_corr_mat(data.frame(a = runif(4, -1, 1), b = runif(4, -1, 1), c = runif(4, -1, 1)))
+#'
 flatten_corr_mat <- function(cormat) {
   ut <- upper.tri(cormat)
   data.frame(
@@ -32,12 +33,14 @@ flatten_corr_mat <- function(cormat) {
 #' @examples
 #' set.seed(0)
 #' get_corr_feats(data.frame(a = runif(4), b = runif(4), c = runif(4)), 0.6)
+#'
+#' @importFrom dplyr .data
 get_corr_feats <- function(features, cor_value = 1) {
   # get correlation matrix
   cormat <- stats::cor(features, method = "spearman")
   # get correlated features
   corr_feats <- flatten_corr_mat(cormat) %>%
-    dplyr::filter(corr >= cor_value)
+    dplyr::filter(.data$corr >= cor_value)
   # return correlated features
   return(corr_feats)
 }
