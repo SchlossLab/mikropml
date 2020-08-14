90c0be52c8eac5382fb6e6d8c27675fd071f4e3b
Kelly Sovacool <kellysovacool@gmail.com>
2020-08-13 20:04:42 -0400

    Rename nfolds to kfold for consistency with ML terms

--- tests/testthat/test-run_ml.R
+++ tests/testthat/test-run_ml.R
@@ -34,7 +34,7 @@ test_that("run_ml works for regLogistic", {
       hyperparameters = mikRopML::default_hyperparams,
       find_feature_importance = FALSE,
       seed = 2019,
-      nfolds = as.integer(2)
+      kfold = as.integer(2)
     ),
     otu_mini_results1
   )
@@ -80,7 +80,7 @@ test_that("run_ml errors if outcome is not binary", {
       ),
       "rf",
       outcome_colname = "dx",
-      nfolds = as.integer(2)
+      kfold = as.integer(2)
     ),
     "A binary outcome variable is required, but this dataset has 3 outcomes"
   )
