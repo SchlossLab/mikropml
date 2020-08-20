42db1473f1a3f08e48fd0e40e24be22f856a178f
Begum Topcuoglu <begum.topcuoglu@merck.com>
2020-08-20 12:52:54 -0400

    RE-run generation of tables for tests

--- tests/testthat/test-calc_aucs.R
+++ tests/testthat/test-calc_aucs.R
@@ -3,14 +3,9 @@ options(warnPartialMatchArgs = FALSE)
 #   warning: get_predictions works
 #   partial argument match of 'contrasts' to 'contrasts.arg'
 
-predictions_sm <- c(
-  0.00916324268270163, 5.50247841835105e-06, 0.992924252456899,
-  7.99185706501504e-09, 0.497920659124481, 3.77475828372553e-15,
-  0.999862709044069, 2.15915501822206e-06, 1.78960513393944e-05,
-  0.0277499865819482, 1.74347802861519e-09, 0.856744054489971
-)
+predictions_sm <- c(9.707251e-02, 3.755703e-01, 2.372989e-02, 9.114752e-01, 8.721929e-01, 1.492075e-05, 7.998901e-01, 8.084112e-01, 9.978739e-01, 3.135994e-01,9.011613e-01, 8.973311e-01, 4.743989e-01, 3.261513e-01, 7.670219e-01, 1.101077e-03, 2.539591e-01, 1.196996e-01, 2.459846e-01, 9.634875e-01, 9.982137e-01, 9.299074e-02, 9.345718e-02, 8.624414e-01, 8.346058e-01,2.630144e-02, 1.839343e-01, 9.137120e-01, 2.205726e-02, 5.216090e-01, 4.293018e-01, 6.287346e-01, 2.273751e-01, 7.503992e-02, 2.090081e-01,4.608598e-01, 3.452780e-01, 2.244180e-01, 2.844962e-02)
 
-outcomes_sm <- c(0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0)
+outcomes_sm <- c(0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1,1)
 tol <- 1e-5
 
 test_that("get_predictions works", {
@@ -26,21 +21,23 @@ test_that("recode_outcome works", {
   )
   expect_equal(
     recode_outcome(test_data_sm, "dx", "normal"),
-    c(1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1)
+    c(1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0)
   )
 })
 test_that("calc_auroc works", {
-  expect_equal(calc_auroc(predictions_sm, outcomes_sm), 0.9)
+  expect_equal(calc_auroc(predictions_sm, outcomes_sm), 0.5052632,
+  tolerance = tol
+  )
 })
 test_that("calc_auprc works", {
-  expect_equal(calc_auprc(predictions_sm, outcomes_sm), 0.7123179,
+  expect_equal(calc_auprc(predictions_sm, outcomes_sm), 0.5447323,
     tolerance = tol
   )
 })
 test_that("calc_aucs works", {
   expect_equal(
     calc_aucs(trained_model_sm1, test_data_sm, "dx", "cancer"),
-    list(auroc = 0.9, auprc = 0.7123179),
+    list(auroc = 0.5052632, auprc = 0.5447323),
     tolerance = tol
   )
 })
--- tests/testthat/test-feature_importance.R
+++ tests/testthat/test-feature_importance.R
@@ -42,7 +42,7 @@ test_that("empty dataframe correct", {
 test_that("permuted auc returns correct value for non-correlated feature", {
   expect_equal(
     find_permuted_auc(trained_model_sm1, test_data_sm, "dx", "Otu00049", "cancer"),
-    c(auc = 0.9, auc_diff = 0.0)
+    c(auc = 0.48573684, auc_diff = 0.01952632)
   )
 })
 
@@ -55,50 +55,25 @@ test_that("permuted auc returns correct value for [fake] correlated feature", {
       "Otu00049|Otu00050",
       "cancer"
     ),
-    c(auc = 0.9, auc_diff = 0.0)
+    c(auc =0.5061578947, auc_diff =-0.0008947368)
   )
 })
 
-feat_imps <- structure(list(auc = c(
-  0.7955, 0.9065, 0.9045, 0.9, 0.9, 0.9,
-  0.8815, 0.906, 0.9465, 0.8935, 0.927, 0.9295, 0.9, 0.9085, 0.903,
-  0.903, 0.9105, 0.903, 0.8975, 0.803, 0.907, 0.9, 0.9, 0.9, 0.93,
-  0.8875, 0.938, 0.9005, 0.9, 0.9035, 0.9, 0.9, 0.9085, 0.8395,
-  0.9045, 0.882, 0.9035, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9,
-  0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9,
-  0.9, 0.9
-), auc_diff = c(
-  0.1045, -0.00649999999999999, -0.00449999999999999,
-  0, 0, 0, 0.0185, -0.00599999999999997, -0.0465, 0.00650000000000001,
-  -0.027, -0.0295, 0, -0.00849999999999999, -0.003, -0.003, -0.0105,
-  -0.00299999999999999, 0.00250000000000001, 0.097, -0.00699999999999997,
-  0, 0, 0, -0.03, 0.0125, -0.038, -0.00049999999999999, 0, -0.0035,
-  0, 0, -0.00849999999999999, 0.0605, -0.00449999999999999, 0.018,
-  -0.0035, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9.99315105491838e-18,
-  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-), names = structure(1:60, .Label = c(
+feat_imps <- structure(list(auc = c(0.5480263, 0.5160526, 0.5808158), auc_diff = c(0.023026316,  0.055000000, -0.009763158), 
+names = structure(1:3, .Label = c(
   "Otu00001",
-  "Otu00002", "Otu00003", "Otu00004", "Otu00005", "Otu00006", "Otu00007",
-  "Otu00008", "Otu00009", "Otu00010", "Otu00011", "Otu00012", "Otu00013",
-  "Otu00014", "Otu00015", "Otu00016", "Otu00017", "Otu00018", "Otu00019",
-  "Otu00020", "Otu00021", "Otu00022", "Otu00023", "Otu00024", "Otu00025",
-  "Otu00026", "Otu00027", "Otu00028", "Otu00029", "Otu00030", "Otu00031",
-  "Otu00032", "Otu00033", "Otu00034", "Otu00035", "Otu00036", "Otu00037",
-  "Otu00038", "Otu00039", "Otu00040", "Otu00041", "Otu00042", "Otu00043",
-  "Otu00044", "Otu00045", "Otu00046", "Otu00047", "Otu00048", "Otu00049",
-  "Otu00050", "Otu00051", "Otu00052", "Otu00053", "Otu00054", "Otu00055",
-  "Otu00056", "Otu00057", "Otu00058", "Otu00059", "Otu00060"
+  "Otu00002", "Otu00003"
 ), class = "factor")), class = "data.frame", row.names = c(
   NA,
-  -60L
+  -3L
 ))
 # get_feature_importance
 test_that("feature importances are correct", {
   expect_equal(get_feature_importance(
-    trained_model_sm1,
-    train_data_sm,
-    test_data_sm,
+    trained_model_mini,
+    train_data_mini,
+    test_data_mini,
     "dx",
     "cancer"
-  ), feat_imps)
+  ), feat_imps, tolerance = 1e-5)
 })
