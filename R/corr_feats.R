0e3b57178af9af24ea33481799d4b7e6c6d275e8
Zena Lapp <zenalapp@gmail.com>
2020-08-24 12:03:19 -0400

    Collapse correlated features in preprocessing resolves #148

--- R/corr_feats.R
+++ R/corr_feats.R
@@ -49,3 +49,42 @@ get_corr_feats <- function(features, corr_thresh = 1) {
       dplyr::filter(.data$corr >= corr_thresh | .data$corr <= -corr_thresh)
   )
 }
+
+#' Group correlated features
+#'
+#' @param corr output of get_corr_feats (pairs of correlated features)
+#' @param features features for ML
+#'
+#' @return vector of correlated features where each element is the group of correlated features separated by pipes (|)
+#' @export
+#' @author Begüm Topçuoğlu, \email{topcuoglu.begum@@gmail.com}
+#' @author Zena Lapp, \email{zenalapp@@umich.edu}
+#'
+group_correlated_features <- function(corr, features) {
+  all_feats <- colnames(features)
+  corr_feats <- unique(c(corr$feature2, corr$feature1))
+  noncorr_feats <- all_feats[!all_feats %in% corr_feats]
+  
+  grps <- as.list(noncorr_feats)
+  accounted_for <- rep(NA, length(all_feats))
+  af_length <- sum(!is.na(accounted_for))
+  c <- length(grps) + 1
+  for (i in corr_feats) {
+    if (i %in% accounted_for) next
+    feats <- unique(c(i, corr$feature1[corr$feature2 == i], corr$feature2[corr$feature1 == i]))
+    new_feats <- T
+    while (new_feats) {
+      len_feats <- length(feats)
+      for (j in feats) {
+        feats <- unique(c(feats, j, corr$feature1[corr$feature2 == j], corr$feature2[corr$feature1 == j]))
+      }
+      new_feats <- length(feats) > len_feats
+    }
+    grps[[c]] <- feats
+    af_length_new <- sum(af_length, length(feats))
+    accounted_for[(af_length + 1):af_length_new] <- feats
+    af_length <- af_length_new
+    c <- c + 1
+  }
+  return(sapply(grps, paste, collapse = "|"))
+}
