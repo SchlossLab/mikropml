981cf06552bf7a050264c9d4776120640972c34f
Kelly Sovacool <kellysovacool@gmail.com>
2020-09-02 17:29:37 -0400

    [WIP] save multiple results files per run

--- examples/snakemake/code/ml.R
+++ examples/snakemake/code/ml.R
@@ -1,5 +1,5 @@
 
-ml_results <- mikRopML::run_pipeline(dataset = readRDS(snakemake@input[['rds']]),
+ml_results <- mikRopML::run_ml(dataset = readRDS(snakemake@input[['rds']]),
                                      method = snakemake@params[['method']],
                                      outcome_colname = 'dx',
                                      outcome_value = 'cancer',
@@ -8,4 +8,6 @@ ml_results <- mikRopML::run_pipeline(dataset = readRDS(snakemake@input[['rds']])
                                      seed = snakemake@params[['seed']],
                                      ncores = snakemake@resources[['ncores']])
 
-saveRDS(ml_results, file = snakemake@output[['rds']])
+saveRDS(ml_results$trained_model, file = snakemake@output[['model']])
+readr::write_csv(ml_results$performance, file = snakemake@output[['perf']])
+readr::write_csv(ml_results$feature_importance, file = snakemake@output[['feat']])
