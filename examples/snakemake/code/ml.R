9368c3e924bcc1ced26a0552ee88736c47769931
Kelly Sovacool <kellysovacool@gmail.com>
2020-09-02 14:15:34 -0400

    Use ncores

--- examples/snakemake/code/ml.R
+++ examples/snakemake/code/ml.R
@@ -1,10 +1,11 @@
 
-otu_data <- readRDS(snakemake@input[['rds']])
-method <- snakemake@params[['method']]
-seed <- snakemake@params[['seed']]
-
-ml_results <- mikRopML::run_pipeline(otu_data, method, outcome_colname = 'dx',
-                                     outcome_value = 'cancer', permute = TRUE,
-                                     nfolds = 5, seed = seed)
+ml_results <- mikRopML::run_pipeline(dataset = readRDS(snakemake@input[['rds']]),
+                                     method = snakemake@params[['method']],
+                                     outcome_colname = 'dx',
+                                     outcome_value = 'cancer',
+                                     permute = TRUE,
+                                     nfolds = 5,
+                                     seed = snakemake@params[['seed']],
+                                     ncores = snakemake@resources[['ncores']])
 
 saveRDS(ml_results, file = snakemake@output[['rds']])
