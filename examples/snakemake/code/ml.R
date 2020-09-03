58d6f35aaa0c954bbea52245c9af54657d3e559b
Kelly Sovacool <kellysovacool@gmail.com>
2020-09-03 11:30:03 -0400

    Fix run_ml usage

--- examples/snakemake/code/ml.R
+++ examples/snakemake/code/ml.R
@@ -1,12 +1,12 @@
-source('log_smk.R')
+source('code/log_smk.R')
 ml_results <- mikRopML::run_ml(dataset = readRDS(snakemake@input[['rds']]),
-                                     method = snakemake@params[['method']],
-                                     outcome_colname = 'dx',
-                                     outcome_value = 'cancer',
-                                     permute = TRUE,
-                                     nfolds = 5,
-                                     seed = snakemake@params[['seed']],
-                                     ncores = snakemake@resources[['ncores']])
+                               method = snakemake@params[['method']],
+                               outcome_colname = 'dx',
+                               outcome_value = 'cancer',
+                               find_feature_importance = TRUE,
+                               kfold = 5,
+                               seed = snakemake@params[['seed']],
+                               ncores = snakemake@resources[['ncores']])
 
 saveRDS(ml_results$trained_model, file = snakemake@output[['model']])
 readr::write_csv(ml_results$performance, file = snakemake@output[['perf']])
