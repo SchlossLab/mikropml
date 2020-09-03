cb9bb25781650d12e53e1773c848e5eec53c7c5c
Kelly Sovacool <kellysovacool@gmail.com>
2020-09-03 13:04:40 -0400

    Breakout variables

--- examples/snakemake/code/ml.R
+++ examples/snakemake/code/ml.R
@@ -1,13 +1,16 @@
 source('code/log_smk.R')
+method <- snakemake@params[['method']]
+seed <- as.numeric(snakemake@params[['seed']])
 data_processed <- readRDS(snakemake@input[['rds']])$dat_transformed
 ml_results <- mikRopML::run_ml(dataset = data_processed,
-                               method = snakemake@params[['method']],
+                               method = method,
                                outcome_colname = 'dx',
                                outcome_value = 'cancer',
                                find_feature_importance = TRUE,
                                kfold = 5,
-                               seed = snakemake@params[['seed']],
-                               ncores = snakemake@resources[['ncores']])
+                               seed = seed,
+                               ncores = as.numeric(snakemake@resources[['ncores']])
+                               )
 
 saveRDS(ml_results$trained_model, file = snakemake@output[['model']])
 readr::write_csv(ml_results$performance, file = snakemake@output[['perf']])
