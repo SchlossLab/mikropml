60d97f1d77c9e549e8afd888ac7b2d3839d1f206
Kelly Sovacool <kellysovacool@gmail.com>
2020-09-22 08:41:18 -0400

    Update parallel tests

--- tests/testthat/test-parallel.R
+++ tests/testthat/test-parallel.R
@@ -4,7 +4,7 @@ test_that("setup_parallel warns", {
         setup_parallel("not_a_number"),
         "`ncores` must be `NA` or a number, but you provided"
     )
-    if (check_package_installed("doParallel")) {
+    if (check_package_installed("doFuture")) {
         expect_warning(
             pc <- setup_parallel(9999999),
             "You specified 9999999 cores, but only"
@@ -12,22 +12,21 @@ test_that("setup_parallel warns", {
     } else {
         expect_warning(
             pc <- setup_parallel(9999999),
-            "The packages `parallel`, `doParallel`, and `foreach` are required for using multiple cores.
- You specified 9999999 cores, but one or more of these packages are not installed.
- Proceeding with only one process"
+            "The packages `future`, `doFuture`, and `foreach` are required for using multiple cores"
         )
     }
+    expect_false(pc)
 })
 test_that("setup_parallel works", {
     expect_false(setup_parallel(NA))
-    if (check_package_installed("doParallel")) {
+    if (check_package_installed("doFuture")) {
         expect_message(pc <- setup_parallel(2), "Using 2 cores for parallel processing.")
+        expect_true(pc)
     } else {
         expect_warning(
             pc <- setup_parallel(2),
-            "The packages `parallel`, `doParallel`, and `foreach` are required for using multiple cores.
- You specified 2 cores, but one or more of these packages are not installed.
- Proceeding with only one process"
+            "The packages `future`, `doFuture`, and `foreach` are required for using multiple cores"
         )
+        expect_false(pc)
     }
 })
