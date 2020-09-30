3810c69563186cc01bbe97def44fe74097203c26
Kelly Sovacool <kellysovacool@gmail.com>
2020-09-30 13:45:08 -0400

    Use geom_boxplot()

--- R/plot.R
+++ R/plot.R
@@ -1,5 +1,7 @@
 #' Plot performance metrics for multiple ML runs with different parameters
 #'
+#' ggplot2 is required to use this function.
+#'
 #' @param performance_df dataframe of performance results from multiple calls to `run_ml()`
 #'
 #' @return a ggplot2 plot
@@ -9,9 +11,9 @@
 #'
 #' @examples
 #' \dontrun{
-#' # call `run_ml` multiple times with different seeds
+#' # call `run_ml()` multiple times with different seeds
 #' results_lst <- lapply(seq(100, 104), function(seed) {
-#'   run_ml(otu_mini, 'regLogistic', seed = seed, kfold = 2)
+#'   run_ml(otu_small, 'regLogistic', seed = seed)
 #'   })
 #' # extract and combine the performance results
 #' perf_df <- lapply(results_lst, function(result) {
@@ -19,9 +21,9 @@
 #'   }) %>%
 #'   dplyr::bind_rows()
 #' # plot the performance results
-#' plot_performance(perf_df)
+#' p <- plot_performance(perf_df)
 #'
-#' # call `run_ml` with different ML methods
+#' # call `run_ml()` with different ML methods
 #' param_grid <- expand.grid(seeds=seq(100, 104),
 #'                           methods=c('regLogistic', 'rf'))
 #' results_mtx <- mapply(function(seed, method) {
@@ -30,25 +32,26 @@
 #' # extract and combine the performance results
 #' perf_df2 <- dplyr::bind_rows(results_mtx['performance',])
 #' # plot the performance results
-#' plot_performance(perf_df2)
+#' p <- plot_performance(perf_df2)
+#'
+#' # you can continue adding layers to customize the plot
+#' p +
+#'   theme_classic() +
+#'   scale_color_brewer(palette = 'Dark2') +
+#'   coord_flip()
 #' }
 plot_performance <- function(performance_df) {
-  if (!check_package_installed('ggplot2')) {
+  if (isFALSE(requireNamespace('ggplot2'))) {
     stop('`ggplot2` is required for `plot_performance()`, but you do not have it installed.')
   }
   performance_df  %>%
     tidy_perf_data() %>%
-    ggplot2::ggplot(aes(x=method, y=value)) +
-      stat_summary(aes(color = metric),
-                   fun.data="mean_se",
-                   fun.args = list(mult=1),
-                   geom = "pointrange",
-                   position = position_dodge(0.2)
-                   ) +
+    ggplot2::ggplot(aes(x=method, y=value, color=metric)) +
+      geom_boxplot() +
       geom_hline(yintercept = 0.5, linetype="dashed") +
     ylim(0, 1) +
-    labs(y='Performance', x='') +
-    coord_flip()
+    labs(y='Performance', x=NULL) +
+    theme(legend.title = element_blank())
 }
 
 #' Tidy the performance dataframe
@@ -63,10 +66,10 @@ tidy_perf_data <- function(performance_df) {
     tidyr::pivot_longer(cols = c(cv_auroc, test_auroc, test_auprc),
                         names_to = "metric") %>%
     dplyr::mutate(
-      metric_pretty = dplyr::case_when(
+      metric = dplyr::case_when(
         metric == "cv_auroc" ~ 'Cross-validation AUROC',
         metric == "test_auroc" ~ 'Testing AUROC',
-        metric == "test_auprc" ~ 'Testing Precision-Recall Curve'
+        metric == "test_auprc" ~ 'Testing AUPRC'
       )
     )
 }
