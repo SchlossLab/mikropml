bca810630407a76e6ec1332d5b9a33595b4643d1
Zena Lapp <zenalapp@gmail.com>
2020-10-14 13:34:36 -0400

    WIP adding multiclass dataset tests

--- R/data.R
+++ R/data.R
@@ -1,4 +1,4 @@
-#' Large OTU abundance dataset
+#' Large OTU abundance dataset with binary class
 #'
 #' A dataset containing relatives abundances of 6,920 OTUs for 490 human stool samples
 #'
@@ -6,6 +6,15 @@
 #' The `dx` column is the diagnosis: healthy or cancerous (colorectal).
 #' All other columns are OTU relative abundances.
 "otu_large"
+
+#' Large OTU abundance dataset with 3 classes
+#'
+#' A dataset containing relatives abundances of 6,920 OTUs for 490 human stool samples
+#'
+#' @format A data frame with 490 rows and 6,921 variables.
+#' The `dx` column is the colon cancer diagnosis: adenoma, carcinoma, or normal.
+#' All other columns are OTU relative abundances.
+"otu_large_multi"
 
 #' Medium-sized OTU abundance dataset
 #'
