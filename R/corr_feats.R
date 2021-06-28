51ad9acc54adbecce8cfb0161efca77514cfd3eb
Kelly Sovacool <kellysovacool@gmail.com>
2021-06-28 17:11:19 -0400

    Use inheritParams for corr_method docs

--- R/corr_feats.R
+++ R/corr_feats.R
@@ -63,8 +63,7 @@ flatten_corr_mat <- function(cormat) {
 #'
 #' @param features data frame with each column as a feature for ML
 #' @param corr_thresh correlation threshold (default: 1)
-#' @param corr_method correlation method. options: spearman, pearson, kendall. (default: spearman)
-#'
+#' @inheritParams get_corr_feats
 #' @return vector of correlated features where each element is a group of
 #'   correlated features separated by pipes (|)
 #' @noRd
