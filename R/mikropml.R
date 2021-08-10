957ea45d859b8f0a9b9e51e8d6111ac20caadd63
Kelly Sovacool <kellysovacool@gmail.com>
2021-08-10 09:42:56 -0400

    Switch orcid urls from http to https

--- R/mikropml.R
+++ R/mikropml.R
@@ -7,12 +7,12 @@
 #'
 #' @section Authors:
 #'
-#' - Begüm D. Topçuoğlu ([ORCID](http://orcid.org/0000-0003-3140-537X))
-#' - Zena Lapp ([ORCID](http://orcid.org/0000-0003-4674-2176))
-#' - Kelly L. Sovacool ([ORCID](http://orcid.org/0000-0003-3283-829X))
-#' - Evan Snitkin ([ORCID](http://orcid.org/0000-0001-8409-278X))
-#' - Jenna Wiens ([ORCID](http://orcid.org/0000-0002-1057-7722))
-#' - Patrick D. Schloss ([ORCID](http://orcid.org/0000-0002-6935-4275))
+#' - Begüm D. Topçuoğlu ([ORCID](https://orcid.org/0000-0003-3140-537X))
+#' - Zena Lapp ([ORCID](https://orcid.org/0000-0003-4674-2176))
+#' - Kelly L. Sovacool ([ORCID](https://orcid.org/0000-0003-3283-829X))
+#' - Evan Snitkin ([ORCID](https://orcid.org/0000-0001-8409-278X))
+#' - Jenna Wiens ([ORCID](https://orcid.org/0000-0002-1057-7722))
+#' - Patrick D. Schloss ([ORCID](https://orcid.org/0000-0002-6935-4275))
 #'
 #' @section See vignettes:
 #'
