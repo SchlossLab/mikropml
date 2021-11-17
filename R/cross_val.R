298e24b5590faa2ed0f9e6fe8f739fda30b675cb
Kelly Sovacool <kellysovacool@gmail.com>
2021-11-17 15:14:52 -0500

    Create keep_groups_in_cv_partitions() & fix logic

--- R/cross_val.R
+++ R/cross_val.R
@@ -25,8 +25,14 @@
 #'   kfold = 5
 #' )
 define_cv <- function(train_data, outcome_colname, hyperparams_list, perf_metric_function, class_probs, kfold = 5, cv_times = 100, groups = NULL, group_partitions = NULL) {
-  keep_groups_in_cv_partitions <- !is.null(groups) & (is.null(group_partitions) | (length(group_partitions[['train']]) >= kfold))
-  if (!keep_groups_in_cv_partitions) {
+
+  if (keep_groups_in_cv_partitions(groups, group_partitions, kfold)) {
+    cvIndex <- create_grouped_k_multifolds(groups,
+      kfold = kfold,
+      cv_times = cv_times
+    )
+    message('Groups will be kept together in CV partitions')
+  } else {
     cvIndex <- caret::createMultiFolds(factor(train_data %>%
       dplyr::pull(outcome_colname)),
     kfold,
@@ -35,12 +41,6 @@ define_cv <- function(train_data, outcome_colname, hyperparams_list, perf_metric
     if (!is.null(groups)) {
       message('Groups will not be kept together in CV partitions because the number of groups in the training set is not larger than `kfold`')
     }
-  } else {
-    cvIndex <- create_grouped_k_multifolds(groups,
-      kfold = kfold,
-      cv_times = cv_times
-    )
-    message('Groups will be kept together in CV partitions')
   }
 
   seeds <- get_seeds_trainControl(hyperparams_list, kfold, cv_times, ncol(train_data))
@@ -89,6 +89,17 @@ get_seeds_trainControl <- function(hyperparams_list, kfold, cv_times, ncol_train
   return(seeds)
 }
 
+#' Whether groups can be kept together in partitions during cross-validation
+#'
+#' @inheritParams define_cv
+#'
+#' @return `TRUE` if possible, `FALSE` otherwise
+#' @export
+#' @author Kelly Sovacool, \email{sovacool@@umich.edu}
+#'
+keep_groups_in_cv_partitions <- function(groups, group_partitions, kfold) {
+  return(!is.null(groups) & ((is.null(group_partitions) & length(unique(groups)) >= kfold) | (length(group_partitions[['train']]) >= kfold)))
+}
 
 #' Splitting into folds for cross-validation when using groups
 #'
