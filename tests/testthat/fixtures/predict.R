c93ca69507f0a4e41002594fc58741d742bee706
Kelly Sovacool <kellysovacool@gmail.com>
2023-01-05 23:43:13 -0600

    Create results-mtx w/ 10 seeds & 2 ml methods

--- tests/testthat/fixtures/predict.R
+++ tests/testthat/fixtures/predict.R
@@ -1,16 +1,29 @@
 library(dplyr)
 devtools::load_all()
-doFuture::registerDoFuture()
-future::plan(future::multicore, workers = 8)
 
-get_sensspec_seed <- function(seed) {
-  ml_result <- run_ml(otu_mini_bin, 'glmnet', seed = seed)
-  sensspec <- get_model_sensspec(ml_result$trained_model,
-                                 ml_result$test_data,
-                                 'dx', 'cancer') %>%
-                                 mutate(seed = seed)
-  return(sensspec)
+results_mtx <- readRDS(testthat::test_path('fixtures', 'results_mtx.Rds'))
+get_sensspec_seed <- function(colnum) {
+    result <- results_mtx[, colnum]
+    trained_model <- result$trained_model
+    test_data <- result$test_data
+    sensspec <- get_model_sensspec(trained_model,
+                                   test_data,
+                                   'dx', 'cancer')
+    return(sensspec)
 }
-sensspec_dat <- furrr::future_map_dfr(seq(100, 110), get_sensspec_seed,
+sensspec_dat <- furrr::future_map_dfr(seq(1, dim(results_mtx)[2]),
+                                      get_sensspec_seed,
                                       .options = furrr::furrr_options(seed = TRUE))
 saveRDS(sensspec_dat, testthat::test_path('fixtures', 'sensspec_dat.Rds'))
+saveRDS(calc_mean_prc(sensspec_dat), testthat::test_path('fixtures', 'sensspec_prc.Rds'))
+saveRDS(calc_mean_roc(sensspec_dat), testthat::test_path('fixtures', 'sensspec_roc.Rds'))
+
+saveRDS(
+    get_model_sensspec(
+        otu_mini_bin_results_glmnet$trained_model,
+        otu_mini_bin_results_glmnet$test_data,
+        'dx',
+        'cancer'
+    ),
+    testthat::test_path('fixtures', 'otu_mini_bin_results_glmnet_sensspec.Rds')
+)
