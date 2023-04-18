8a18c1d341533695bc6118eb5fb0a64c07cfbb84
shah-priyal <shahpri@umich.edu>
2023-04-18 15:35:03 -0400

    added roxygen docstring for impute function

--- R/impute.R
+++ R/impute.R
@@ -1,3 +1,13 @@
+#' Replace NA values with the median value of the column for continious variables in the dataset
+#'
+#' @param transformed_cont Data frame that may include NA values in one or more columns
+#'
+#' @return Data frame that has no NA values in continious numeric columns
+#'
+#' @examples
+#' transformed_cont <- impute(transformed_cont)
+#' train_data <- impute(train_data)
+#' test_data <- impute(test_data)
 impute <- function(transformed_cont) {
   sapply_fn <- select_apply("sapply")
   cl <- sapply_fn(transformed_cont, function(x) {
@@ -23,5 +33,5 @@ impute <- function(transformed_cont) {
       )
     )
   }
-  return (transformed_cont)git 
+  return (transformed_cont) 
 }
