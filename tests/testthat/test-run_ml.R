c64736e58b0287b029588c6f944892d304285a35
Kelly Sovacool <kelly.sovacool@nih.gov>
2025-07-21 19:19:13 -0400

    test: skip some ml tests on ci for run time
    also use snapshot for rf test

--- tests/testthat/test-run_ml.R
+++ tests/testthat/test-run_ml.R
@@ -80,24 +80,21 @@ test_that("run_ml works for linear regression", {
 
 test_that("run_ml works for random forest with grouping & feature importance", {
   skip_on_cran()
-  expect_equal_ml_results(
-    mikropml::run_ml(otu_mini_bin,
+  skip_on_ci()
+  expect_snapshot(
+    res <- mikropml::run_ml(otu_mini_bin,
       "rf",
       outcome_colname = "dx",
       find_feature_importance = TRUE,
       seed = 2019,
       cv_times = 2,
       groups = otu_mini_group
-    ),
-    otu_mini_bin_results_rf,
-    tolerance = 1e-3
-  ) %>%
-    suppressWarnings() %>%
-    suppressMessages()
+    ))
 })
 
 test_that("run_ml works for svmRadial", {
   skip_on_cran()
+  skip_on_ci()
   expect_equal_ml_results(
     mikropml::run_ml(otu_mini_bin,
       "svmRadial",
@@ -114,6 +111,7 @@ test_that("run_ml works for svmRadial", {
 
 test_that("run_ml works for xgbTree", {
   skip_on_cran()
+  skip_on_ci()
   skip_on_os(c("linux", "windows")) # bug in xgboost package: https://discuss.xgboost.ai/t/colsample-by-tree-leads-to-not-reproducible-model-across-machines-mac-os-windows/1709
   expect_equal_ml_results(
     mikropml::run_ml(
@@ -133,6 +131,7 @@ test_that("run_ml works for xgbTree", {
 
 test_that("run_ml works for rpart2", {
   skip_on_cran()
+  skip_on_ci()
   expect_equal_ml_results(
     mikropml::run_ml(otu_mini_bin,
       "rpart2",
