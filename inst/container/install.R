6644c8502d1f4bf9887dbd640290499f08ba0e31
Kelly Sovacool, PhD <kelly-sovacool@users.noreply.github.com>
2026-08-08 15:16:58 -0400

    create container & push to ghcr (#364)

---
+++ inst/container/install.R
@@ -0,0 +1,9 @@
+#!/usr/bin/env Rscript
+
+# see issue #352
+remotes::install_version('xgboost','v1.7')
+
+# install all dependencies, but do not upgrade already-installed pkg versions
+remotes::install_local('/opt/mikropml', dependencies=TRUE, upgrade=FALSE)
+
+readr::write_csv(tibble::as_tibble(installed.packages()), '/data/r-packages.csv')
